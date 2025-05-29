b588d3d7dc1f919528270eb5d7638726cc7cf15a
carlielbot <cvl0703@gmail.com>
2025-05-29 17:01:14 +0200

    ajustando el .env y el vendor

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+# Instala dependencias si no existe vendor
+if [ ! -d "vendor" ]; then
+  composer install
+
+  # Modifica la línea de $except en VerifyCsrfToken.php
+  sed -i "s|protected \$except = .*;|protected \$except = ['api/*'];|" vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+
+fi
+
+# Copia .env si no existe
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+
+
+fi
+
 # Espera a que la base de datos esté disponible
 until php artisan migrate --force; do
   echo "Esperando a la base de datos..."
